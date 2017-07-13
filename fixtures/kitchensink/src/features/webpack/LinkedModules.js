e83fd113970afc7c4c54ae48155d69cfba1ae8fa
Ashleigh Arnaud <ashleigh.arnaud@gmail.com>
2017-07-13 16:58:29 -0500

    Added the markerclusterer file and some json.

--- fixtures/kitchensink/src/features/webpack/LinkedModules.js
+++ fixtures/kitchensink/src/features/webpack/LinkedModules.js
@@ -7,14 +7,14 @@
  * of patent rights can be found in the PATENTS file in the same directory.
  */
 
-import Inferno from 'inferno';
-import './assets/style.css';
-import { test, version } from 'test-integrity';
+import Inferno from 'inferno'
+import './assets/style.css'
+import { test, version } from 'test-integrity'
 
 export default () => {
-  const v = version();
+  const v = version()
   if (!test() || v !== '2.0.0') {
-    throw new Error('Functionality test did not pass.');
+    throw new Error('Functionality test did not pass.')
   }
-  return <p id="feature-linked-modules">{v}</p>;
-};
+  return <p id='feature-linked-modules'>{v}</p>
+}
--- src/App.js
+++ src/App.js
@@ -1,20 +1,20 @@
-import { version } from 'inferno';
-import Component from 'inferno-component';
-import './App.css';
+import { version } from 'inferno'
+import Component from 'inferno-component'
+import './App.css'
 
 class App extends Component {
-  render() {
+  render () {
     return (
-      <div className="App">
-        <div className="App-header">
+      <div className='App'>
+        <div className='App-header'>
           <h2>{`Welcome to Inferno ${version}`}</h2>
         </div>
-        <p className="App-intro">
+        <p className='App-intro'>
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
       </div>
-    );
+    )
   }
 }
 
-export default App;
+export default App
