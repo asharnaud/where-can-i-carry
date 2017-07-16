135413f58983805fc8b68a2090a265717bad1eb1
Ashleigh Arnaud <ashleigh.arnaud@gmail.com>
2017-07-15 21:37:25 -0500

    Added the marker clusters with the info window.

--- src/map.js
+++ src/map.js
@@ -4,47 +4,43 @@ import './markerclusterer.js'
 import $ from './zepto-1.2.0.js'
 
 var companyObj = []
-console.log(companyObj)
+var companyName = ''
 
 function fetchJSONdata (data) {
   data.forEach(function (item) {
     companyObj.push({company: item.company, lat: item.lat, lng: item.lng, allowsOpenCarry: item.allowsOpenCarry, allowsConcealedCarry: item.allowsConcealedCarry})
   })
-  return companyObj
+  buildLocations()
+  buildInfoWindow()
 }
 
 $.getJSON('../data/locations.json', fetchJSONdata)
 
-var locations = companyObj.map(function (obj) {
-  console.log(obj)
-  return [{
-    lat: obj.lat,
-    lng: obj.lng
-  }]
-})
+var locations = []
 
-console.log(locations)
+function buildLocations () {
+  companyObj.map(function (obj) {
+    return locations.push({
+      lat: obj.lat,
+      lng: obj.lng
+    })
+  })
+}
 
-var contentString = '<div id="content">'+
-     '<div id="siteNotice">'+
-     '</div>'+
-     '<h1 id="firstHeading" class="firstHeading"> + companyName</h1>'+
-     '<div id="bodyContent">'+
-     '<p><b>Uluru</b>, also referred to as <b>Ayers Rock</b>, is a large ' +
-     'sandstone rock formation in the southern part of the '+
-     'Northern Territory, central Australia. It lies 335&#160;km (208&#160;mi) '+
-     'south west of the nearest large town, Alice Springs; 450&#160;km '+
-     '(280&#160;mi) by road. Kata Tjuta and Uluru are the two major '+
-     'features of the Uluru - Kata Tjuta National Park. Uluru is '+
-     'sacred to the Pitjantjatjara and Yankunytjatjara, the '+
-     'Aboriginal people of the area. It has many springs, waterholes, '+
-     'rock caves and ancient paintings. Uluru is listed as a World '+
-     'Heritage Site.</p>'+
-     '<p>Attribution: Uluru, <a href="https://en.wikipedia.org/w/index.php?title=Uluru&oldid=297882194">'+
-     'https://en.wikipedia.org/w/index.php?title=Uluru</a> '+
-     '(last visited June 22, 2009).</p>'+
-     '</div>'+
-     '</div>';
+function buildInfoWindow () {
+  companyName = companyObj.company
+  console.log(companyName)
+  return companyName
+}
+
+var contentString = '<div id="content">' +
+     '<div id="siteNotice">' +
+     '</div>' +
+     '<h1>' + companyName + '</h1>' +
+     '<div id="bodyContent">' +
+     '<p>This is the Info Window!</p>' +
+     '</div>' +
+     '</div>'
 
 function initGMap () {
   var map = new google.maps.Map(document.getElementById('map'), {
